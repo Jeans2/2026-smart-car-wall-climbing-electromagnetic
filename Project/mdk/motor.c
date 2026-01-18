1e91020c4f8062f8463941fd390d8a9ce62652cd
尚伯乐 <2242893021@qq.com>
2026-01-19 01:10:38 +0800

    添加电机函数

--- Project/mdk/motor.c
+++ Project/mdk/motor.c
@@ -1,1 +1,49 @@
 #include "motor.h"
+
+
+
+void motor_init(void)
+{
+    gpio_init(DIR_L, GPO, GPIO_HIGH, GPO_PUSH_PULL);                            // GPIO 初始化为输出 默认上拉输出高
+    pwm_init(PWM_L, 17000, 0);                                                  // PWM 通道初始化频率 17KHz 占空比初始为 0
+
+    gpio_init(DIR_R, GPO, GPIO_HIGH, GPO_PUSH_PULL);                            // GPIO 初始化为输出 默认上拉输出高
+    pwm_init(PWM_R, 17000, 0);    
+	
+}
+
+void set_pwm_motor_L(int16 duty)
+{
+	  if(duty>9900){duty=9900;}
+	  if(duty<-9900){duty=-9900;}
+		
+		if(duty>0)
+		{
+		gpio_set_level(DIR_L, GPIO_HIGH);
+		pwm_set_duty(PWM_L,duty);
+		}
+		else
+		{
+		gpio_set_level(DIR_L, GPIO_LOW);
+		pwm_set_duty(PWM_L,-duty);
+		}
+}
+
+
+void set_pwm_motor_R(int16 duty)
+{
+	  if(duty>9900){duty=9900;}
+	  if(duty<-9900){duty=-9900;}
+		
+		if(duty>0)
+		{
+		gpio_set_level(DIR_R, GPIO_HIGH);
+		pwm_set_duty(PWM_R,duty);
+		}
+		else
+		{
+		gpio_set_level(DIR_R, GPIO_LOW);
+		pwm_set_duty(PWM_R,-duty);
+		}
+}
+
--- Project/mdk/motor.h
+++ Project/mdk/motor.h
@@ -3,9 +3,13 @@
 
 #include "bsp_system.h"
 
+#define DIR_L               (IO_P64 )
+#define PWM_L               (PWMA_CH4P_P66)
+#define DIR_R               (IO_P60 )
+#define PWM_R               (PWMA_CH2P_P62)
 
 
-
-
-
+void motor_init(void);
+void set_pwm_motor_L(int16 duty);
+void set_pwm_motor_R(int16 duty);
 #endif
